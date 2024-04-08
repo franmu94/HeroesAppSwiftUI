6ac287a618274cb4d44e3de8d69b56ad72651bd8
franmu94 <fmalourarte@gmail.com>
2024-04-08 08:31:55 +0200

    Initial Commit

---
+++ HeroesAppSwiftUI/HeroesAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroesAppSwiftUIApp.swift
+//  HeroesAppSwiftUI
+//
+//  Created by Fran Malo on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct HeroesAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
