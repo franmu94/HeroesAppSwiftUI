3d93a0d1b5f242d8f064bd66a6065ffd0d9e2c79
franmu94 <fmalourarte@gmail.com>
2024-04-16 14:09:40 +0200

    tapview integrada

---
+++ HeroesAppSwiftUI/HeroesTabView.swift
@@ -0,0 +1,18 @@
+//
+//  HeroesTabView.swift
+//  HeroesAppSwiftUI
+//
+//  Created by Fran Malo on 15/4/24.
+//
+
+import SwiftUI
+
+struct HeroesTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeroesTabView()
+}
---
+++ HeroesAppSwiftUI/ListaFiltradaPoder.swift
@@ -0,0 +1,18 @@
+//
+//  ListaFiltradaPoder.swift
+//  HeroesAppSwiftUI
+//
+//  Created by Fran Malo on 15/4/24.
+//
+
+import SwiftUI
+
+struct ListaFiltradaPoder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListaFiltradaPoder()
+}
